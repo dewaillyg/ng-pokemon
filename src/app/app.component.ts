f9e3d0eb458f908a9ef2836d5784fc2ddfeed803
guillaume dewailly <dewailly.freelance@gmail.com>
2025-04-13 12:48:00 +0200

    add datas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{ pokemonList[1] }}!</h1>
 
     <router-outlet />
   `,
   standalone: false,
   styles: []
 })
-export class AppComponent {
-  title: string = 'ng-pokemon';
+
+export class AppComponent implements OnInit {
+  pokemonList: string[] = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemonName: string) {
+    console.log(`Vous avez clické sur le Pokemon ${pokemonName}`);
+  }
 }
